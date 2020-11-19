6b32bd2789c43de2050a05cd6a111b15ae69c4b4
Lionel Bonzoumet <lionel.bonzoumet@gmail.com>
2020-11-19 12:42:11 +0100

    feat(Header): add login, signup, my account, logout buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 import PricesIndexes from "./components/PricesIndexes";
 import RightMenu from "./components/RightMenu";
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,31 @@
+import { Button, Col, Row, Container } from "react-bootstrap";
 import "./Header.css";
 
 function Header(props) {
+  const { isLogged } = props;
+
   return (
     <div className="header">
-      <h1>COUNT OF MONEY HEADER</h1>
+      <Container>
+        <Row>
+          <Col lg={9}>
+            <h1>COUNT OF MONEY</h1>
+          </Col>
+          <Col lg={3}>
+            {isLogged ? (
+              <div>
+                <Button>My account</Button>
+                <Button>Logout</Button>
+              </div>
+            ) : (
+              <div>
+                <Button>Login</Button>
+                <Button>Signup</Button>
+              </div>
+            )}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
