07165c22f8e6c9f114c9c03ddfb231e8ace75843
Lionel Bonzoumet <lionel.bonzoumet@gmail.com>
2020-11-19 15:43:42 +0100

    feat(App): add router in main content
    - add links in header buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Container, Row, Col } from "react-bootstrap";
 import PricesIndexes from "./components/PricesIndexes";
-import RightMenu from "./components/RightMenu";
 import News from "./components/News";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -14,26 +16,25 @@ function App() {
           <Header />
         </Row>
         <Container>
-          <Row>
-            <Col lg={9} className="main-content">
-              <h2>Main content</h2>
-              <Col>
-                <Row>
+          <Col className="main-content">
+            <h2>Main content</h2>
+            <Row>
+              <Switch>
+                <Route path="/login">
+                  <Login />
+                </Route>
+                <Route path="/signup">
+                  <SignUp />
+                </Route>
+                <Route path="/">
                   <PricesIndexes />
-                </Row>
-                <Row>
                   <News />
-                </Row>
-              </Col>
-            </Col>
-            <Col lg={3}>
-              <RightMenu />
-            </Col>
-          </Row>
+                </Route>
+              </Switch>
+            </Row>
+          </Col>
         </Container>
-        <Row>
-          <Footer />
-        </Row>
+        <Footer />
       </Container>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Button, Col, Row, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header(props) {
@@ -8,27 +9,35 @@ function Header(props) {
     <div className="header">
       <Container>
         <Row>
-          <Col lg={8}>
-            <h1>COUNT OF MONEY</h1>
+          <Col lg={8} className="d-flex justify-content-start">
+            <Link to="/">
+              <h1>COUNT OF MONEY</h1>
+            </Link>
           </Col>
           <Col lg={4}>
             {isLogged ? (
               <div className="d-flex justify-content-end">
+                <Link to="/user">
+                  <Button>
+                    <i className="fas fa-address-card"></i> My account
+                  </Button>
+                </Link>
                 <Button>
-                  <i class="fas fa-address-card"></i> My account
-                </Button>
-                <Button>
-                  <i class="fas fa-sign-out-alt"></i> Logout
+                  <i className="fas fa-sign-out-alt"></i> Logout
                 </Button>
               </div>
             ) : (
               <div className="d-flex justify-content-end">
-                <Button>
-                  <i class="fas fa-sign-in-alt"></i> Login
-                </Button>
-                <Button>
-                  <i class="fas fa-user-plus"></i> Signup
-                </Button>
+                <Link to="/login">
+                  <Button>
+                    <i className="fas fa-sign-in-alt"></i> Login
+                  </Button>
+                </Link>
+                <Link to="/signup">
+                  <Button>
+                    <i className="fas fa-user-plus"></i> Signup
+                  </Button>
+                </Link>
               </div>
             )}
           </Col>
