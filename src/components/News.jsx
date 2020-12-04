d0546c9de13e4b6933302fb7191480322cf37c11
Lionel <lionel@declique.fr>
2020-12-04 14:57:10 +0100

    feat(news): add loading spinner and tags to articles

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,20 +3,26 @@ import { Card } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import RssParser from "rss-parser";
 import moment from "moment";
+import Spinner from "./Spinner/Spinner";
 
 import "./News.css";
 
 function News(props) {
   const [rssData, setRssData] = useState([]);
   const [rssIsLoaded, setRssIsLoaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     let parser = new RssParser();
     const rssFeed = async () => {
-      let feed = await parser.parseURL("https://cointelegraph.com/rss");
+      setIsLoading(true);
+      let feed = await parser.parseURL(
+        "https://cors-anywhere.herokuapp.com/https://cointelegraph.com/rss"
+      );
       setRssData(feed);
       console.log(feed);
       setRssIsLoaded(true);
+      setIsLoading(false);
     };
     rssFeed();
   }, []);
@@ -24,9 +30,10 @@ function News(props) {
   return (
     <Card className="news">
       <Card.Header>
-        <h3>Last Cryptocurrencies News - {rssData.title}</h3>
+        <h2>{isLoading ? "Loading news..." : rssData.title}</h2>
       </Card.Header>
       <div className="d-flex flex-wrap card-container">
+        {isLoading ? <Spinner /> : null}
         {rssIsLoaded
           ? rssData.items.slice(0, 6).map((item, i) => {
               return (
@@ -38,6 +45,13 @@ function News(props) {
                   </Card.Body>
                   <Card.Footer>
                     <small className="text-muted">
+                      <p>
+                        {item.categories
+                          .map((cat, i) => {
+                            return <span key={i}>{cat}</span>;
+                          })
+                          .reduce((prev, curr) => [prev, " - ", curr])}
+                      </p>
                       {moment(item.pubDate).format("MMMM Do YYYY, h:mm:ss a")}
                     </small>
                   </Card.Footer>
