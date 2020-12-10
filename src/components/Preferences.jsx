162d03d5db96a0503a4cbc5603b552572cc96e62
Abel <abel.de-smedt @epitech.eu>
2020-12-10 16:38:35 +0100

    [*] add modif Inputs in Preferences

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -41,29 +41,91 @@ class Preferences extends React.Component {
     super(props);
 
     this.state = {
-      checkedItems: new Map(),
+      UserName: "TestUserName",
+      Mail: "TestMail",
+      FirstName: "TestFirstName",
+      LastName: "TestLastName",
+      checkedCrypto: new Map(),
     }
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeMail = this.handleChangeMail.bind(this);
+    this.changeUserName = this.changeUserName.bind(this);
+    this.ChangeLastName = this.ChangeLastName.bind(this);
+    this.ChangeFirstName = this.ChangeFirstName.bind(this);
+    this.Change = this.Change.bind(this);
   }
 
-  handleChange(event) {
+  handleChangeMail(event) {
+    this.setState({ Mail: event.target.value });
+  }
+
+  ChangeLastName(event) {
+    this.setState({ LastName: event.target.value });
+  }
+
+  ChangeFirstName(event) {
+    this.setState({ FirstName: event.target.value });
+  }
+
+  changeUserName(event) {
+    this.setState({ UserName: event.target.value });
+  }
+  
+  Change(event) {
     const item = event.target.name;
     const isChecked = event.target.checked;
-    this.setState(prevState => ({ checkedItems: prevState.checkedItems.set(item, isChecked) }));
+    this.setState(prevState => ({ checkedCrypto: prevState.checkedCrypto.set(item, isChecked) }));
   }
 
   render() {
     return (
       <React.Fragment>
-        {
-          CurrentCryptos.map(item => (
-            <label class ="dispCheckboxes" key={item.key}>
-                {item.name}
-                <Checkbox name={item.name} checked={this.state.checkedItems.get(item.name)} onChange={this.handleChange} />
-            </label>
-          ))
-        }
+        <div class="preferences">
+          <p>Preferences</p>
+          <label>
+              UserName :<br></br>
+              <input
+                type="text"
+                value={this.state.UserName}
+                onChange={this.changeUserName}
+              />
+          </label>
+          <label>
+              Mail :<br></br>
+              <input
+                type="text"
+                value={this.state.Mail}
+                onChange={this.handleChangeMail}
+              />
+          </label>
+          <label>
+              FirstName :<br></br>
+              <input
+                type="text"
+                value={this.state.FirstName}
+                onChange={this.ChangeFirstName}
+              />
+          </label>
+          <label>
+              LastName :<br></br>
+              <input
+                type="text"
+                value={this.state.LastName}
+                onChange={this.ChangeLastName}
+              />
+          </label>
+          <label>
+              Your Cryptos :<br></br>
+          </label>
+          {
+            CurrentCryptos.map(item => (
+              <label class ="dispCheckboxes" key={item.key}>
+                  <span class="span"><Checkbox name={item.name} checked={this.state.checkedCrypto.get(item.name)} onChange={this.Change} /></span>
+                  {item.name}
+              </label>
+            ))
+          }
+        </div>
       </React.Fragment>
     );
   }
